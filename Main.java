f4ac131672cf9c371979380c77c0d1a90495f3a6
Giovanna <giovanna.bellonia@dtidigital.com.br>
2020-10-07 13:51:11 -0300

    apagando dollar e franc e passando para money

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ class Main {
       Tests tests = new Tests();
       tests.testMultiplication();
       tests.testEquality();
-      tests.testFrancMultiplication();
       tests.testCurrency();
     }
   }
--- Money.java
+++ Money.java
@@ -1,13 +1,13 @@
-abstract class Money  {
+class Money  {
     protected int amount;
     protected String currency;
  
     static Money dollar(int amount)  {
-       return new Dollar(amount, "USD");
+       return new Money(amount, "USD");
     }
  
     static Money franc(int amount) {
-       return new Franc(amount, "CHF");
+       return new Money(amount, "CHF");
     }
  
     Money(int amount, String currency) {
@@ -17,13 +17,16 @@ abstract class Money  {
     
     public boolean equals(Object object) {
        Money money = (Money) object;
-       return amount == money.amount && getClass().equals(money.getClass());
-    }   
+       return amount == money.amount && currency().equals(money.currency());
+    }
+     
+    Money times(int multiplier) {
+       return new Money(amount * multiplier, currency);
+    } 
  
      String currency() {
        return currency;
     }
-     
-    abstract Money times(int multiplier);  
+  
  }
  
