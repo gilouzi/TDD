d472d06e30c76382e2e9f9030074aac04f9acbbb
Giovanna <giovanna.bellonia@dtidigital.com.br>
2020-10-07 11:56:48 -0300

    Criacao teste inicial

---
+++ Tests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+class Tests {
+
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+     }
+  }
